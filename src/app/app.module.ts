fd6ad4dec71da3a5bc0f6d71977e9fa9a2e2323e
Yuriy Serbin <serbingeorg@hotmail.com>
2020-03-30 21:11:33 +0300

    how to create your component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponent } from './my/my.component';
+import { My2Component } from './my2/my2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    My2Component
   ],
   imports: [
     BrowserModule,
