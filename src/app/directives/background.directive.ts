273198bcee170f2d34c42256ee923f3d83120e65
Yuriy Serbin <serbingeorg@hotmail.com>
2020-04-18 18:11:32 +0300

    passing parameters

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,20 +1,23 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 @Directive({
     selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
-
+    @Input('appBackground') hoverColor = 'green';
+    @Input() defaultColor = 'transparent';
     @HostBinding('style.backgroundColor') backgroung: string;
+
     constructor(private element: ElementRef, private renderer: Renderer2) { }
 
     ngOnInit() {
+        this.backgroung = this.defaultColor;
     }
 
     @HostListener('mouseenter') mouseEnter() {
-        this.backgroung = 'green';
+        this.backgroung = this.hoverColor;
     }
 
     @HostListener('mouseleave') mouseLeave() {
-        this.backgroung = 'trasparent';
+        this.backgroung = this.defaultColor;
     }
 }
