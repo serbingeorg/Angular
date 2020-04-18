918b3bf2792f01057d0ee386844520177c7f4207
Yuriy Serbin <serbingeorg@hotmail.com>
2020-04-18 16:42:59 +0300

    Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { from } from 'rxjs';
-import { AddCarComponent } from './add-car/add-car.component';
+import { BackgroundDirective } from './directives/background.directive';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
+    BackgroundDirective
+
 
   ],
   imports: [
---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+@Directive({
+    selector: '[appBackground]'
+})
+export class BackgroundDirective implements OnInit {
+    constructor(private element: ElementRef) { }
+
+    ngOnInit() {
+        console.log(this.element);
+    }
+}
