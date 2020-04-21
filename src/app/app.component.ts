7677fcba6a4c809e7e53bf2b61cb0ae61881de48
Yuriy Serbin <serbingeorg@hotmail.com>
2020-04-21 13:01:29 +0300

    cheking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class = 'col-xs-8 col-xs-offset-2' >
+    <h2>{{ name }}</h2>
+    <h2>{{ name | lowercase }}</h2>
+      </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Create your directive.';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-
-  onClick(item: number) {
-    this.current = item;
-  }
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+    {
+      xyz: 3,
+      numbers: [1, 2, 3]
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { BackgroundDirective } from './directives/background.directive';
-
-
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundDirective,
+    AppComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
