5546eda1739a3773ac9ef48b06cb1a9e6b6c33d3
Yuriy Serbin <serbingeorg@hotmail.com>
2020-05-12 13:55:51 +0300

    Group, validators, asyncvalidators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -21,18 +22,49 @@ export class AppComponent implements OnInit {
     text: 'Нет'
   }];
 
+
+  charsCount = 5;
+
   form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      pass: new FormControl('', Validators.required),
+      user: new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email], this.checkForEmail),
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
+      }),
       country: new FormControl('ru'),
       answer: new FormControl('no')
     });
   }
   onSubmit() {
     console.log('Submoted!', this.form);
+  }
+
+  checkForLength(control: FormControl) {
+    if (control.value.length <= this.charsCount) {
+      return {
+        // tslint:disable-next-line:object-literal-key-quotes
+        'lengthError': true
+      };
+    }
+    return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@mail.ru') {
+          resolve({
+            // tslint:disable-next-line:object-literal-key-quotes
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
   }
 
 }
