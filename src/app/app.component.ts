a7576000b6bf2c25b03052d9f07b5b3f8988d660
Yuriy Serbin <serbingeorg@hotmail.com>
2020-05-15 17:02:38 +0300

    fucking map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { CarsService } from './cars.service';
 
+
+
+interface Cars {
+  name: string;
+  color: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  cars = [];
+  cars: Cars[] = [];
 
 
   constructor(private carsServise: CarsService) { }
 
   loadCars() {
-    this.carsServise.getCars().subscribe((response) => {
-      console.log(response);
-    });
+    this.carsServise.getCars()
+      .subscribe((cars: Cars[]) => {
+        this.cars = cars;
+      });
   }
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// tslint:disable-next-line:quotemark
+import { map } from "rxjs/operators";
+
+
+
 
 @Injectable()
 export class CarsService {
     constructor(private http: HttpClient) { }
 
     getCars() {
-        return this.http.get('http://localhost:3000/cars');
+        return this.http.get('http://localhost:3000/cars')
+            .pipe(map(res => res));
     }
 }
